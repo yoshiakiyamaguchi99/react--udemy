c6186ca7b899c264c1dbb0cbc589e5eacebbe703
yoshiakiyamaguchi <yama.goo.yama.goo@gmail.com>
2021-01-02 18:43:22 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@
 
 
 function App() {
-  const greet = "Hi,tom"
-  const dom = <h1 className=""></h1>
-  return dom;
+  const profiles = [
+    {name: "taro", age: 10 },
+    {name: "Hanako", age: 5},
+    {name: "noName"}
+  ]
+  return (
+    <div>
+    {
+      profiles.map((profile, index) => {
+        return   <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
+
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi! i am {props.name}, and {props.age} years old. </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
